304a1f2052ec3fdd9717e712085a87f0586aae65
bhh <haibhut@gmail.com>
2016-12-13 00:14:16 +0700

    First

--- Develop/ClinicGui.py
+++ Develop/ClinicGui.py
@@ -1,9 +1,10 @@
 # import Tkinter as tk   # python3
-import Tkinter as tk   # python
+import Tkinter as tk  # python
 
 TITLE_FONT = ("Helvetica", 18, "bold")
 
-class ClinicApp (tk.Tk):
+
+class ClinicApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
@@ -22,27 +23,66 @@ class ClinicApp (tk.Tk):
         self.show_frame("LoginPage")
 
     def show_frame(self, page_name):
-        frame = self.frames[page_name]
+        frame = self.frame[page_name]
         frame.tkraise()
 
-class LoginPage(tk.Frame):
 
+class LoginPage(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
+        tk.Frame.__init__(self, parent)
         self.controller = controller
+
+        bgclr = "#282828"
+        fgclr = "#cecece"
+        # clr = '#004a95'
+
+        self.users = [("kiman", "phongmachbacsi")]
+
+        self.user_label = tk.Label(self, text="Username", font=("blod", 15), bg=bgclr, fg=fgclr)
+        self.user_label.place(x=20, y=40)
+
+        self.user_entry = tk.Entry(self, bg=bgclr, width=40, font=10, bd=5)
+        self.user_entry.place(x=20, y=80)
+
+        self.password_label = tk.Label(self, text="Password", font=("blod", 15), bg=bgclr, fg=fgclr)
+        self.password_label.place(x=20, y=120)
+
+        self.password_entry = tk.Entry(self, bg=bgclr, fg="white", width=40, font=10, show="*", bd=5)
+        self.password_entry.place(x=20, y=160)
+
+        self.warn_label = tk.Label(self, font=("blod", 10), bg=bgclr)
+        self.warn_label.place(x=80, y=200)
+
+        self.login_button = tk.Button(self, text="Login", width="40", font=10, command=self.login())
+        self.login_button.place(x=80, y=200)
+
+    def login(self):
+        if (self.user_entry.get(), self.password_entry.get()) in self.users:
+            self.controller.show_frame("PationPage")
+        else:
+            self.warn_label.config(text="Invalid username or Password", fg="red")
 
 
 class PatientPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        label = tk.Label(self, text="This is page 1", font=TITLE_FONT)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("LoginPage"))
+        button.pack()
 
 
 class PharmaPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-
+        label = tk.Label(self, text="This is page 2", font=TITLE_FONT)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("LoginPage"))
+        button.pack()
 
 
 if __name__ == "__main__":
