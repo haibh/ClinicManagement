c60a04ceedd501ae4ad971fd2285939fdcc67d20
bhh <haibhut@gmail.com>
2016-12-23 09:49:35 +0700

    First

--- backend.py
+++ backend.py
@@ -12,9 +12,27 @@ def connect():
 def insert(title, author, year, isbn):
     conn = sqlite3.connect("book.db")
     cur = conn.cursor()
-    cur.execute()
+    cur.execute("INSERT INTO book VALUES (null,?,?,?,?)", (title, author, year, isbn))
     conn.commit()
     conn.close()
 
+def view():
+    conn = sqlite3.connect("book.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * from book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="", author="", year="", isbn=""):
+    conn = sqlite3.connect("book.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? or isbn=?",(title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
 
 connect()
+insert("Sach","BHH",1985,91234981234)
+print(view())
